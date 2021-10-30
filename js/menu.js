47c212b79e528e3e121e32cb5580ee7cdc9edd43
AgarwalsKulfiRoll <shaikhjameel462@gmail.com>
2021-10-30 11:33:18 +0530

    updated files

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
+let menu = document.querySelector('.menu-section');
 let menubtn = document.getElementById('menubtn');
+let closebtn = document.getElementById('close');
 
 menubtn.addEventListener('click', function() {
-    let menu = document.querySelector('.menu-section');
     menu.classList.toggle('activemenu');
+});
+
+closebtn.addEventListener('click', function() {
+    if (menu.classList == 'menu-section activemenu') {
+        menu.classList = 'menu-section';
+    } else {
+        menu.classList = 'menu-section activemenu';
+    }
 });
